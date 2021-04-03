a792d8c416983d3fef48587197a3f29250222a20
Lilian Kasem <likasem@microsoft.com>
2021-04-03 13:57:09 -0700

    Write scripts and update doc

--- setup/service_principal.sh
+++ setup/service_principal.sh
@@ -1,9 +1,53 @@
 #!/bin/bash
 
-SUBSCRIPTION_ID=
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+RESET=`tput sgr0`
 
-az login
+inputs=$@
 
-az account set --subscription="SUBSCRIPTION_ID"
+for ARGUMENT in "$@"
+do
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)
 
-az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$SUBSCRIPTION_ID"
+    case "$KEY" in
+      s)                  SUBSCRIPTION_ID=${VALUE} ;;
+      u)                         USERNAME=${VALUE} ;;
+      f)                TF_VARS_FILE_PATH=${VALUE} ;;
+      *)
+    esac
+done
+
+createServicePrincipal() {
+  # Set service princial name
+  USERNAME="terraform-sp-$USERNAME-$RANDOM"
+
+  # Login to Azure
+  az login
+
+  # Set Azure subscription
+  az account set --subscription=$SUBSCRIPTION_ID
+
+  # Create service principal
+  SERVICE_PRINCIPAL=$(az ad sp create-for-rbac --name $USERNAME --role="Contributor" --scopes="/subscriptions/$SUBSCRIPTION_ID")
+
+  SP_NAME=$(echo $SERVICE_PRINCIPAL | jq -r .displayName)
+  SP_TENANT=$(echo $SERVICE_PRINCIPAL | jq -r .tenant)
+  SP_CLIENT_ID=$(echo $SERVICE_PRINCIPAL | jq -r .appId)
+  SP_CLIENT_SECRET=$(echo $SERVICE_PRINCIPAL | jq -r .password)
+
+  echo "${GREEN}Service Principal Name:           ${RESET}$SP_NAME"
+  echo "${GREEN}Service Principal Tenant:         ${RESET}$SP_TENANT"
+  echo "${GREEN}Service Principal Id:             ${RESET}$SP_CLIENT_ID"
+  echo "${GREEN}Service Principal Secret:         ${RESET}$SP_CLIENT_SECRET"
+
+  # Append tfvars file
+  echo "tenant_id=\"$SP_TENANT\""                 >> $TF_VARS_FILE_PATH
+  echo "subscription_id=\"$SUBSCRIPTION_ID\""     >> $TF_VARS_FILE_PATH
+  echo "client_id=\"$SP_CLIENT_ID\""              >> $TF_VARS_FILE_PATH
+  echo "client_secret=\"$SP_CLIENT_SECRET\""      >> $TF_VARS_FILE_PATH
+}
+
+# Check Arguments
+[[ $inputs < 2 ]] && { exit 1; } || createServicePrincipal
--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,4 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "multitstate"
-    storage_account_name  = "multitstate14594"
-    container_name        = "multitstate"
-    key                   = "terraform.tfstate"
-    # set ARM_ACCESS_KEY in environment || ARM_CLIENT_ID ARM_CLIENT_SECRET ARM_SUBSCRIPTION_ID ARM_TENANT_ID 
   }
 }
